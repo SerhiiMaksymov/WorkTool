fe6ff2af51521d0998d6e65233d7340bd84a59a6
Serhii Maksymov <maksimovsergey08@gmail.com>
2023-01-26 14:55:08 +0200

    add AvaloniaUiDesktopModule

---
+++ WorkTool.Core.Modules.AvaloniaUi.Desktop/Modules/AvaloniaUiDesktopModule.cs
@@ -0,0 +1,19 @@
+﻿namespace WorkTool.Core.Modules.AvaloniaUi.Modules;
+
+public class AvaloniaUiDesktopModule : ModularSystemModule
+{
+    private static readonly DependencyInjector MainDependencyInjector;
+
+    private const string IdString = "b05188e3-4519-43f8-ba9d-2372e69ce331";
+
+    public static readonly Guid IdValue = Guid.Parse(IdString);
+
+    static AvaloniaUiDesktopModule()
+    {
+        var register = new ReadOnlyDependencyInjectorRegister();
+        register.RegisterConfiguration<AvaloniaUiDesktopDependencyInjectorConfiguration>();
+        MainDependencyInjector = register.Build();
+    }
+
+    public AvaloniaUiDesktopModule() : base(IdValue, MainDependencyInjector) { }
+}
